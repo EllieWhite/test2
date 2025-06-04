e9fdee884e86126e7185e176516a8ad55646345d
Ellie <diana.lyutaya12@gmail.com>
2025-06-04 23:59:43 +0300

    add styles and fix deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,25 @@
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; 
 import { LazyAbout } from '@/pages/about/About.lazy';
 import { LazyShop } from './pages/shop/Shop.lazy';
 import { Suspense } from 'react';
+
 const root = document.getElementById('root');
 
-if(!root) {
-    throw new Error('root not found')
+if (!root) {
+    throw new Error('root not found');
 }
 
-const container = createRoot(root)
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                path: '/about',
-                element: <LazyAbout />
-            },
-            {
-                path: '/shop',
-                element: <Suspense fallback={'Loading...'}><LazyShop /></Suspense>
-            }
-        ]
-    }
-])
+const container = createRoot(root);
 
 container.render(
-    <RouterProvider router={router}/>
-)
+    <HashRouter>
+        <Routes> 
+            <Route path="/" element={<App />}>
+                <Route path="/about" element={<LazyAbout />} />
+                <Route path="/shop" element={<Suspense fallback={'Loading...'}><LazyShop /></Suspense>} />
+            </Route>
+        </Routes>
+    </HashRouter>
+);
